c80d259af0b4c530e0efa6b5fbe1630d84b0097b
James Forshaw <forshaw@google.com>
2016-10-18 17:46:41 +0100

    Remove unused using.

--- NtApiDotNet/NtSecurity.cs
+++ NtApiDotNet/NtSecurity.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Win32.SafeHandles;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
