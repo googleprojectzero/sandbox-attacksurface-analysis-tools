6cb8ea90840819d8bafc08c2cb3dd4ad1ca66b22
James Forshaw <forshaw@google.com>
2017-05-22 18:34:13 +0100

    Added NtToken to list.

--- NtApiDotNet/NtObject.cs
+++ NtApiDotNet/NtObject.cs
@@ -1044,6 +1044,8 @@ namespace NtApiDotNet
                     return new NtJob(DuplicateHandle());
                 case "key":
                     return new NtKey(DuplicateHandle());
+                case "token":
+                    return new NtToken(DuplicateHandle());
                 default:
                     return Duplicate();
             }
