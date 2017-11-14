af82a81634f962f226b5c09d67ff19d32b7abe5d
James Forshaw <forshaw@google.com>
2017-11-14 15:21:51 +0000

    Cleanup bit flags to remove unneeded values.

--- ManagedHandleUtils/NativeBridge.cs
+++ ManagedHandleUtils/NativeBridge.cs
@@ -22,24 +22,46 @@ namespace SandboxAnalysisUtils
 {
     public static class NativeBridge
     {
-        static void AddEnumToDictionary(Dictionary<uint, String> access, Type enumType)
+        static bool IsValidMask(uint mask, uint valid_mask)
+        {
+            if (mask == 0)
+            {
+                return false;
+            }
+
+            // Filter out generic access etc.
+            if ((mask & ~valid_mask) != 0)
+            {
+                return false;
+            }
+
+            // Check if the mask only has a single bit set.
+            if ((mask & (mask - 1)) != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        static void AddEnumToDictionary(Dictionary<uint, String> access, Type enumType, uint valid_mask)
         {
             Regex re = new Regex("([A-Z])");
 
             foreach(uint mask in Enum.GetValues(enumType))
             {
-                if (mask != 0)
+                if (IsValidMask(mask, valid_mask))
                 {
                     access.Add(mask, re.Replace(Enum.GetName(enumType, mask), " $1").Trim());
                 }
             }
         }
 
-        static Dictionary<uint, String> GetMaskDictionary(Type enumType)
+        static Dictionary<uint, String> GetMaskDictionary(NtType type)
         {
             Dictionary<uint, String> access = new Dictionary<uint, String>();
                         
-            AddEnumToDictionary(access, enumType);
+            AddEnumToDictionary(access, type.AccessRightsType, type.ValidAccess.Access);
 
             return access;
         }
@@ -49,7 +71,7 @@ namespace SandboxAnalysisUtils
         
         public static void EditSecurity(IntPtr hwnd, NtObject handle, string object_name, bool read_only)
         {
-            Dictionary<uint, String> access = GetMaskDictionary(handle.NtType.AccessRightsType);
+            Dictionary<uint, String> access = GetMaskDictionary(handle.NtType);
 
             using (SecurityInformationImpl impl = new SecurityInformationImpl(object_name, handle, access,
                handle.NtType.GenericMapping, read_only))
@@ -60,7 +82,7 @@ namespace SandboxAnalysisUtils
 
         public static void EditSecurity(IntPtr hwnd, string name, SecurityDescriptor sd, NtType type)
         {
-            Dictionary<uint, String> access = GetMaskDictionary(type.AccessRightsType);
+            Dictionary<uint, String> access = GetMaskDictionary(type);
             using (var impl = new SecurityInformationImpl(name, sd, access, type.GenericMapping))
             {
                 EditSecurity(hwnd, impl);
