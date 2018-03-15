5f7e46298c0275e0ba5a63bba1cfce17ba61a921
James Forshaw <forshaw@google.com>
2018-03-15 13:44:12 +0000

    Added missing RWX constant.

--- NtApiDotNet/NtVirtualMemory.cs
+++ NtApiDotNet/NtVirtualMemory.cs
@@ -86,6 +86,7 @@ namespace NtApiDotNet
         WriteCopy = 0x08,
         Execute = 0x10,
         ExecuteRead = 0x20,
+        ExecuteReadWrite = 0x40,
         ExecuteWriteCopy = 0x80,
         Guard = 0x100,
         NoCache = 0x200,
