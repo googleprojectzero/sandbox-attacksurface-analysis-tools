f7370497030186dc42c20751a007359599f33a8b
James Forshaw <tyranid@gmail.com>
2018-03-27 12:05:20 +0100

    Implemented Set-NtFileReparsePoint cmdlet.

--- NtObjectManager/NtFileCmdlets.cs
+++ NtObjectManager/NtFileCmdlets.cs
@@ -106,8 +106,8 @@ namespace NtObjectManager
 
             return path;
         }
-
-        private static string ResolvePath(SessionState state, string path, bool win32_path)
+        
+        internal static string ResolvePath(SessionState state, string path, bool win32_path)
         {
             if (win32_path)
             {
@@ -569,6 +569,120 @@ namespace NtObjectManager
             {
                 return file.GetReparsePoint();
             }
+        }
+    }
+
+    /// <summary>
+    /// <para type="synopsis">Open and reads the reparse point buffer for file.</para>
+    /// <para type="description">This cmdlet opens a existing NT file object and reads out the reparse point buffer data. 
+    /// The absolute path to the object in the NT object manager name space can be specified. 
+    /// It's also possible to open the object relative to an existing object by specified the -Root parameter.
+    /// To simplify calling it's also possible to specify the path in a Win32 format when using the -Win32Path parameter.</para>
+    /// </summary>
+    /// <example>
+    ///   <code>Set-NtFileReparsePoint \??\C:\ABC \??\C:\XYZ</code>
+    ///   <para>Sets the symbolic link for file \??\C:\ABC to point to \??\C:\XYZ.</para>
+    /// </example>
+    /// <example>
+    ///   <code>Set-NtFileReparsePoint \??\C:\ABC \??\C:\XYZ "BLAH BLAH"</code>
+    ///   <para>Sets the symbolic link for file \??\C:\ABC to point to \??\C:\XYZ with an explicit print name.</para>
+    /// </example>
+    /// <example>
+    ///   <code>Set-NtFileReparsePoint \??\C:\ABC \??\C:\XYZ -Directory</code>
+    ///   <para>Sets the symbolic link for directory \??\C:\ABC to point to \??\C:\XYZ.</para>
+    /// </example>
+    /// <example>
+    ///   <code>Set-NtFileReparsePoint C:\ABC ..\..\XYZ -Win32Path</code>
+    ///   <para>Sets the symbolic link for file C:\ABC to point to C:\XYZ using Win32 paths.</para>
+    /// </example>
+    /// <example>
+    ///   <code>Set-NtFileReparsePoint \??\C:\ABC ..\..\XYZ -Relative</code>
+    ///   <para>Sets the symbolic link for file \??\C:\ABC to point to ..\..\XYZ using a relative path.</para>
+    /// </example>
+    /// <example>
+    ///   <code>Set-NtFileReparsePoint \??\C:\ABC \??\C:\XYZ -MountPoint</code>
+    ///   <para>Sets the mount point for file \??\C:\ABC to point to \??\C:\XYZ.</para>
+    /// </example>
+    /// <example>
+    ///   <code>Set-NtFileReparsePoint \??\C:\ABC -ReparseBuffer $rp</code>
+    ///   <para>Sets the reparse buffer for file \??\C:\ABC using a raw reparse buffer.</para>
+    /// </example>
+    [Cmdlet(VerbsCommon.Set, "NtFileReparsePoint", DefaultParameterSetName = "Symlink")]
+    public class SetNtFileReparsePointCmdlet : NewNtFileCmdlet
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SetNtFileReparsePointCmdlet()
+        {
+            Access = FileAccessRights.GenericWrite;
+            Disposition = FileDisposition.OpenIf;
+        }
+
+        /// <summary>
+        /// <para type="description">Specify creating a mount point.</para>
+        /// </summary>
+        [Parameter(Mandatory = true, ParameterSetName = "MountPoint")]
+        public SwitchParameter MountPoint { get; set; }
+
+        /// <summary>
+        /// <para type="description">Specify creating a mount point.</para>
+        /// </summary>
+        [Parameter(Mandatory = true, ParameterSetName = "MountPoint", Position = 1), 
+            Parameter(Mandatory = true, ParameterSetName = "Symlink", Position = 1)]
+        public string TargetPath { get; set; }
+
+        /// <summary>
+        /// <para type="description">Specify a print name for the reparse point.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "MountPoint", Position = 2),
+            Parameter(ParameterSetName = "Symlink", Position = 2)]
+        public string PrintName { get; set; }
+
+        /// <summary>
+        /// <para type="description">Specify the symlink target should be a relative path.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "Symlink")]
+        public SwitchParameter Relative { get; set; }
+
+        /// <summary>
+        /// <para type="description">Specify the a raw reparse point buffer.</para>
+        /// </summary>
+        [Parameter(Mandatory = true, ParameterSetName = "ReparseBuffer", Position = 1)]
+        public ReparseBuffer ReparseBuffer { get; set; }
+
+        /// <summary>
+        /// Method to create an object from a set of object attributes.
+        /// </summary>
+        /// <param name="obj_attributes">The object attributes to create/open from.</param>
+        /// <returns>The newly created object.</returns>
+        protected override object CreateObject(ObjectAttributes obj_attributes)
+        {
+            NtToken.EnableEffectivePrivilege(TokenPrivilegeValue.SeCreateSymbolicLinkPrivilege);
+            Options |= FileOpenOptions.OpenReparsePoint;
+
+            if (ParameterSetName != "ReparseBuffer")
+            {
+                string target_path = Relative ? TargetPath : ResolvePath(SessionState, TargetPath, Win32Path);
+                switch (ParameterSetName)
+                {
+                    case "MountPoint":
+                        Directory = true;
+                        ReparseBuffer = new MountPointReparseBuffer(target_path, PrintName);
+                        break;
+                    case "Symlink":
+                        ReparseBuffer = new SymlinkReparseBuffer(target_path, string.IsNullOrEmpty(PrintName)
+                            ? target_path : PrintName, Relative ? SymlinkReparseBufferFlags.Relative : SymlinkReparseBufferFlags.None);
+                        break;
+                }
+            }
+
+            using (NtFile file = (NtFile)base.CreateObject(obj_attributes))
+            {
+                file.SetReparsePoint(ReparseBuffer);
+            }
+
+            return null;
         }
     }
 
