e3ee105d23fd56c91a320756ab932d1d5cac1b78
James Forshaw <forshaw@google.com>
2018-07-31 13:14:16 +0100

    Reworked Get-NtAccessMask cmdlet.

--- NtObjectManager/NtSecurityCmdlets.cs
+++ NtObjectManager/NtSecurityCmdlets.cs
@@ -279,29 +279,85 @@ namespace NtApiDotNet
         }
     }
 
+#pragma warning disable 1591
+    /// <summary>
+    /// <para type="description">Enumeration for specific access type mapping.</para>
+    /// </summary>
+    public enum SpecificAccessType
+    {
+        None,
+        ALPCPort,
+        DebugObject,
+        Desktop,
+        Directory,
+        Event,
+        File,
+        Device,
+        IoCompletion,
+        Job,
+        Key,
+        Mutant,
+        Partition,
+        Process,
+        RegistryTransaction,
+        Section,
+        Semaphore,
+        Session,
+        SymbolicLink,
+        Thread,
+        Token,
+        TmTx,
+        WindowStation,
+    }
+#pragma warning restore 1591
+
     /// <summary>
     /// <para type="synopsis">Convert a specific object access to an AccessMask or GenericAccess.</para>
     /// <para type="description">This cmdlet allows you to convert a specific object access to an
     /// AccessMask or GenericAccess for use in general functions.</para>
     /// </summary>
     /// <example>
-    ///   <code>Get-NtAccessMask -Process DupHandle</code>
+    ///   <code>Get-NtAccessMask -ProcessAccess DupHandle</code>
     ///   <para>Get the Process DupHandle access right as an AccessMask</para>
     /// </example>
     /// <example>
-    ///   <code>Get-NtAccessMask -Process DupHandle -ToGenericAccess</code>
+    ///   <code>Get-NtAccessMask -ProcessAccess DupHandle -ToGenericAccess</code>
     ///   <para>Get the Process DupHandle access right as a GenericAccess value</para>
     /// </example>
-    [Cmdlet(VerbsCommon.Get, "NtAccessMask")]
-    public class GetNtAccessMaskCmdlet : Cmdlet
+    /// <example>
+    ///   <code>Get-NtAccessMask -AccessMask 0xFF -ToSpecificAccess Process</code>
+    ///   <para>Convert a raw access mask to a process access mask.</para>
+    /// </example>
+    /// <example>
+    ///   <code>Get-NtAccessMask -AccessControlEntry $sd.Dacl[0] -ToSpecificAccess Thread</code>
+    ///   <para>Get the access mask from a security descriptor ACE and map to thread access.</para>
+    /// </example>
+    /// <example>
+    ///   <code>$sd.Dacl | Get-NtAccessMask -ToSpecificAccess Thread</code>
+    ///   <para>Get the access mask from a list of security descriptor ACEs and map to thread access.</para>
+    /// </example>
+    [Cmdlet(VerbsCommon.Get, "NtAccessMask", DefaultParameterSetName = "FromMask")]
+    public class GetNtAccessMaskCmdlet : PSCmdlet
     {
-        private AccessMask MapGeneric(string typename, AccessMask access_mask)
+        private static NtType GetTypeObject(SpecificAccessType type)
+        {
+            if (type == SpecificAccessType.ALPCPort)
+            {
+                return NtType.GetTypeByType<NtAlpc>();
+            }
+            else
+            {
+                return NtType.GetTypeByName(type.ToString(), false);
+            }
+        }
+
+        private AccessMask MapGeneric(SpecificAccessType specific_type, AccessMask access_mask)
         {
             if (!MapGenericRights)
             {
                 return access_mask;
             }
-            NtType type = NtType.GetTypeByName(typename, false);
+            NtType type = GetTypeObject(specific_type);
             System.Diagnostics.Debug.Assert(type != null);
             return type.MapGenericRights(access_mask);
         }
@@ -309,8 +365,108 @@ namespace NtApiDotNet
         /// <summary>
         /// <para type="description">Specify a raw access mask.</para>
         /// </summary>
-        [Parameter(Position = 0)]
+        [Parameter(Position = 0, ParameterSetName = "FromMask", Mandatory = true)]
         public AccessMask AccessMask { get; set; }
+        /// <summary>
+        /// <para type="description">Specify File access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromFile", Mandatory = true)]
+        public FileAccessRights FileAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify File Directory access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromFileDir", Mandatory = true)]
+        public FileDirectoryAccessRights FileDirectoryAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify IO Completion access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromIoCompletion", Mandatory = true)]
+        public IoCompletionAccessRights IoCompletionAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Mutant access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromMutant", Mandatory = true)]
+        public MutantAccessRights MutantAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Semaphore access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromSemaphore", Mandatory = true)]
+        public SemaphoreAccessRights SemaphoreAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Registry Transaction access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromRegTrans", Mandatory = true)]
+        public RegistryTransactionAccessRights RegistryTransactionAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify ALPC Port access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromAlpc", Mandatory = true)]
+        public AlpcAccessRights AlpcPortAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Section access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromSection", Mandatory = true)]
+        public SectionAccessRights SectionAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Key access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromKey", Mandatory = true)]
+        public KeyAccessRights KeyAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Event access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromEvent", Mandatory = true)]
+        public EventAccessRights EventAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Symbolic Link access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromSymbolicLink", Mandatory = true)]
+        public SymbolicLinkAccessRights SymbolicLinkAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Token access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromToken", Mandatory = true)]
+        public TokenAccessRights TokenAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Generic access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromGeneric", Mandatory = true)]
+        public GenericAccessRights GenericAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Directory access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromDirectory", Mandatory = true)]
+        public DirectoryAccessRights DirectoryAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Thread access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromThread", Mandatory = true)]
+        public ThreadAccessRights ThreadAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Debug Object access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromDebugObject", Mandatory = true)]
+        public DebugAccessRights DebugObjectAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Job access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromJob", Mandatory = true)]
+        public JobAccessRights JobAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify Process access rights.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromThread", Mandatory = true)]
+        public ProcessAccessRights ProcessAccess { get; set; }
+        /// <summary>
+        /// <para type="description">Specify mandatory label policy.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromMandatoryLabel", Mandatory = true)]
+        public MandatoryLabelPolicy ManadatoryLabelPolicy { get; set; }
+        /// <summary>
+        /// <para type="description">Specify an ACE to extract the mask to map.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromAce", Mandatory = true, ValueFromPipeline = true, Position = 0)]
+        public Ace AccessControlEntry { get; set; }
         /// <summary>
         /// <para type="description">Return access as GenericAccess.</para>
         /// </summary>
@@ -325,134 +481,56 @@ namespace NtApiDotNet
         /// <para type="description">Return access as specific access type based on the NtType.</para>
         /// </summary>
         [Parameter]
-        public string ToSpecificAccess { get; set; }
+        public SpecificAccessType ToSpecificAccess { get; set; }
         /// <summary>
         /// <para type="description">Specify that any generic rights should be mapped to type specific rights.</para>
         /// </summary>
         [Parameter]
         public SwitchParameter MapGenericRights { get; set; }
+
         /// <summary>
-        /// <para type="description">Specify File access rights.</para>
+        /// Constructor.
         /// </summary>
-        [Parameter]
-        public FileAccessRights FileAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify File Directory access rights.</para>
-        /// </summary>
-        [Parameter]
-        public FileDirectoryAccessRights FileDirectoryAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify IO Completion access rights.</para>
-        /// </summary>
-        [Parameter]
-        public IoCompletionAccessRights IoCompletionAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Mutant access rights.</para>
-        /// </summary>
-        [Parameter]
-        public MutantAccessRights MutantAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Semaphore access rights.</para>
-        /// </summary>
-        [Parameter]
-        public SemaphoreAccessRights SemaphoreAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Registry Transaction access rights.</para>
-        /// </summary>
-        [Parameter]
-        public RegistryTransactionAccessRights RegistryTransactionAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify ALPC Port access rights.</para>
-        /// </summary>
-        [Parameter]
-        public AlpcAccessRights AlpcPortAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Section access rights.</para>
-        /// </summary>
-        [Parameter]
-        public SectionAccessRights SectionAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Key access rights.</para>
-        /// </summary>
-        [Parameter]
-        public KeyAccessRights KeyAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Event access rights.</para>
-        /// </summary>
-        [Parameter]
-        public EventAccessRights EventAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Symbolic Link access rights.</para>
-        /// </summary>
-        [Parameter]
-        public SymbolicLinkAccessRights SymbolicLinkAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Token access rights.</para>
-        /// </summary>
-        [Parameter]
-        public TokenAccessRights TokenAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Generic access rights.</para>
-        /// </summary>
-        [Parameter]
-        public GenericAccessRights GenericAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Directory access rights.</para>
-        /// </summary>
-        [Parameter]
-        public DirectoryAccessRights DirectoryAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Thread access rights.</para>
-        /// </summary>
-        [Parameter]
-        public ThreadAccessRights ThreadAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Debug Object access rights.</para>
-        /// </summary>
-        [Parameter]
-        public DebugAccessRights DebugObjectAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Job access rights.</para>
-        /// </summary>
-        [Parameter]
-        public JobAccessRights JobAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify Process access rights.</para>
-        /// </summary>
-        [Parameter]
-        public ProcessAccessRights ProcessAccess { get; set; }
-        /// <summary>
-        /// <para type="description">Specify mandatory label policy.</para>
-        /// </summary>
-        [Parameter]
-        public MandatoryLabelPolicy ManadatoryLabelPolicy { get; set; }
+        public GetNtAccessMaskCmdlet()
+        {
+            ToSpecificAccess = SpecificAccessType.None;
+        }
 
         /// <summary>
         /// Overridden ProcessRecord
         /// </summary>
         protected override void ProcessRecord()
         {
-            AccessMask mask = AccessMask;
+            AccessMask mask = 0;
 
-            mask |= MapGeneric("File", FileAccess);
-            mask |= MapGeneric("File", FileDirectoryAccess);
-            mask |= MapGeneric("IoCompletion", IoCompletionAccess);
-            mask |= MapGeneric("Mutant", MutantAccess);
-            mask |= MapGeneric("Semaphore", SemaphoreAccess);
-            mask |= MapGeneric("RegistryTransaction", RegistryTransactionAccess);
-            mask |= MapGeneric("ALPC Port", AlpcPortAccess);
-            mask |= MapGeneric("Section", SectionAccess);
-            mask |= MapGeneric("Key", KeyAccess);
-            mask |= MapGeneric("Event", EventAccess);
-            mask |= MapGeneric("SymbolicLink", SymbolicLinkAccess);
-            mask |= MapGeneric("Token", TokenAccess);
-            mask |= GenericAccess;
-            mask |= MapGeneric("Directory", DirectoryAccess);
-            mask |= MapGeneric("Thread", ThreadAccess);
-            mask |= MapGeneric("DebugObject", DebugObjectAccess);
-            mask |= MapGeneric("Job", JobAccess);
-            mask |= MapGeneric("Process", ProcessAccess);
-            mask |= (uint)ManadatoryLabelPolicy;
+            switch (ParameterSetName)
+            {
+                case "FromAce":
+                    mask = AccessControlEntry.Mask;
+                    break;
+                default:
+                    mask = AccessMask;
+                    mask |= MapGeneric(SpecificAccessType.File, FileAccess);
+                    mask |= MapGeneric(SpecificAccessType.File, FileDirectoryAccess);
+                    mask |= MapGeneric(SpecificAccessType.IoCompletion, IoCompletionAccess);
+                    mask |= MapGeneric(SpecificAccessType.Mutant, MutantAccess);
+                    mask |= MapGeneric(SpecificAccessType.Semaphore, SemaphoreAccess);
+                    mask |= MapGeneric(SpecificAccessType.RegistryTransaction, RegistryTransactionAccess);
+                    mask |= MapGeneric(SpecificAccessType.ALPCPort, AlpcPortAccess);
+                    mask |= MapGeneric(SpecificAccessType.Section, SectionAccess);
+                    mask |= MapGeneric(SpecificAccessType.Key, KeyAccess);
+                    mask |= MapGeneric(SpecificAccessType.Event, EventAccess);
+                    mask |= MapGeneric(SpecificAccessType.SymbolicLink, SymbolicLinkAccess);
+                    mask |= MapGeneric(SpecificAccessType.Token, TokenAccess);
+                    mask |= GenericAccess;
+                    mask |= MapGeneric(SpecificAccessType.Directory, DirectoryAccess);
+                    mask |= MapGeneric(SpecificAccessType.Thread, ThreadAccess);
+                    mask |= MapGeneric(SpecificAccessType.DebugObject, DebugObjectAccess);
+                    mask |= MapGeneric(SpecificAccessType.Job, JobAccess);
+                    mask |= MapGeneric(SpecificAccessType.Process, ProcessAccess);
+                    mask |= (uint)ManadatoryLabelPolicy;
+                    break;
+            }
 
             if (ToGenericAccess)
             {
@@ -462,17 +540,13 @@ namespace NtApiDotNet
             {
                 WriteObject(mask.ToMandatoryLabelPolicy());
             }
-            else if (String.IsNullOrEmpty(ToSpecificAccess))
+            else if (ToSpecificAccess == SpecificAccessType.None)
             {
                 WriteObject(mask);
             }
             else
             {
-                NtType type = NtType.GetTypeByName(ToSpecificAccess, false);
-                if (type == null)
-                {
-                    throw new ArgumentException($"'{ToSpecificAccess}' is not a valid NT type name");
-                }
+                NtType type = GetTypeObject(ToSpecificAccess);
                 WriteObject(mask.ToSpecificAccess(type.AccessRightsType));
             }
         }
