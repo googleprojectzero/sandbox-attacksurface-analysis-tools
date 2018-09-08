e4eef3918fb085e725c7d8b4c96d5a626ac025e0
James Forshaw <tyranid@gmail.com>
2018-09-08 21:28:14 +0100

    Added support for RtlQueryPackageClaims.

--- NtApiDotNet/NtSecurity.cs
+++ NtApiDotNet/NtSecurity.cs
@@ -346,6 +346,162 @@ namespace NtApiDotNet
         }
     }
 
+    [StructLayout(LayoutKind.Sequential)]
+    public class PsPkgClaim
+    {
+        public ulong Flags;
+        public ulong Origin;
+    }
+
+    public enum AppModelPolicy_Type
+    {
+        LifecycleManager = 0x1,
+        AppDataAccess = 0x2,
+        WindowingModel = 0x3,
+        DllSearchOrder = 0x4,
+        Fusion = 0x5,
+        NonWindowsCodeLoading = 0x6,
+        ProcessEnd = 0x7,
+        BeginThreadInit = 0x8,
+        DeveloperInformation = 0x9,
+        CreateFileAccess = 0xA,
+        ImplicitPackageBreakaway_Internal = 0xB,
+        ProcessActivationShim = 0xC,
+        AppKnownToStateRepository = 0xD,
+        AudioManagement = 0xE,
+        PackageMayContainPublicComRegistrations = 0xF,
+        PackageMayContainPrivateComRegistrations = 0x10,
+        LaunchCreateProcessExtensions = 0x11,
+        ClrCompat = 0x12,
+        LoaderIgnoreAlteredSearchForRelativePath = 0x13,
+        ImplicitlyActivateClassicAAAServersAsIU = 0x14,
+        ComClassicCatalog = 0x15,
+        ComUnmarshaling = 0x16,
+        ComAppLaunchPerfEnhancements = 0x17,
+        ComSecurityInitialization = 0x18,
+        RoInitializeSingleThreadedBehavior = 0x19,
+        ComDefaultExceptionHandling = 0x1A,
+        ComOopProxyAgility = 0x1B,
+        AppServiceLifetime = 0x1C,
+        WebPlatform = 0x1D,
+        WinInetStoragePartitioning = 0x1E,
+        IndexerProtocolHandlerHost = 0x1F,
+        LoaderIncludeUserDirectories = 0x20,
+        ConvertAppContainerToRestrictedAppContainer = 0x21,
+        PackageMayContainPrivateMapiProvider = 0x22,
+        AdminProcessPackageClaims = 0x23,
+        RegistryRedirectionBehavior = 0x24,
+        BypassCreateProcessAppxExtension = 0x25,
+        KnownFolderRedirection = 0x26,
+        PrivateActivateAsPackageWinrtClasses = 0x27,
+        AppPrivateFolderRedirection = 0x28,
+        GlobalSystemAppDataAccess = 0x29,
+        ConsoleHandleInheritance = 0x2A,
+        ConsoleBufferAccess = 0x2B,
+        ConvertCallerTokenToUserTokenForDeployment = 0x2C
+    };
+
+    public enum AppModelPolicy_PolicyValue
+    {
+        None = 0,
+        LifecycleManager_Unmanaged = 0x10000,
+        LifecycleManager_ManagedByPLM = 0x10001,
+        LifecycleManager_ManagedByEM = 0x10002,
+        AppDataAccess_Allowed = 0x20000,
+        AppDataAccess_Denied = 0x20001,
+        WindowingModel_Hwnd = 0x30000,
+        WindowingModel_CoreWindow = 0x30001,
+        WindowingModel_LegacyPhone = 0x30002,
+        WindowingModel_None = 0x30003,
+        DllSearchOrder_Traditional = 0x40000,
+        DllSearchOrder_PackageGraphBased = 0x40001,
+        Fusion_Full = 0x50000,
+        Fusion_Limited = 0x50001,
+        NonWindowsCodeLoading_Allowed = 0x60000,
+        NonWindowsCodeLoading_Denied = 0x60001,
+        ProcessEnd_TerminateProcess = 0x70000,
+        ProcessEnd_ExitProcess = 0x70001,
+        BeginThreadInit_RoInitialize = 0x80000,
+        BeginThreadInit_None = 0x80001,
+        DeveloperInformation_UI = 0x90000,
+        DeveloperInformation_None = 0x90001,
+        CreateFileAccess_Full = 0xA0000,
+        CreateFileAccess_Limited = 0xA0001,
+        ImplicitPackageBreakaway_Allowed = 0xB0000,
+        ImplicitPackageBreakaway_Denied = 0xB0001,
+        ImplicitPackageBreakaway_DeniedByApp = 0xB0002,
+        ProcessActivationShim_None = 0xC0000,
+        ProcessActivationShim_PackagedCWALauncher = 0xC0001,
+        AppKnownToStateRepository_Known = 0xD0000,
+        AppKnownToStateRepository_Unknown = 0xD0001,
+        AudioManagement_Unmanaged = 0xE0000,
+        AudioManagement_ManagedByPBM = 0xE0001,
+        PackageMayContainPublicComRegistrations_Yes = 0xF0000,
+        PackageMayContainPublicComRegistrations_No = 0xF0001,
+        PackageMayContainPrivateComRegistrations_None = 0x100000,
+        PackageMayContainPrivateComRegistrations_PrivateHive = 0x100001,
+        LaunchCreateProcessExtensions_None = 0x110000,
+        LaunchCreateProcessExtensions_RegisterWithPsm = 0x110001,
+        LaunchCreateProcessExtensions_RegisterWithDesktopAppX = 0x110002,
+        ClrCompat_Others = 0x120000,
+        ClrCompat_ClassicDesktop = 0x120001,
+        ClrCompat_Universal = 0x120002,
+        ClrCompat_PackagedDesktop = 0x120003,
+        LoaderIgnoreAlteredSearchForRelativePath_False = 0x130000,
+        LoaderIgnoreAlteredSearchForRelativePath_True = 0x130001,
+        ImplicitlyActivateClassicAAAServersAsIU_Yes = 0x140000,
+        ImplicitlyActivateClassicAAAServersAsIU_No = 0x140001,
+        ComClassicCatalog_MachineHiveAndUserHive = 0x150000,
+        ComClassicCatalog_MachineHiveOnly = 0x150001,
+        ComUnmarshaling_ForceStrongUnmarshaling = 0x160000,
+        ComUnmarshaling_ApplicationManaged = 0x160001,
+        ComAppLaunchPerfEnhancements_Enabled = 0x170000,
+        ComAppLaunchPerfEnhancements_Disabled = 0x170001,
+        ComSecurityInitialization_ApplicationManaged = 0x180000,
+        ComSecurityInitialization_SystemManaged = 0x180001,
+        RoInitializeSingleThreadedBehavior_ASTA = 0x190000,
+        RoInitializeSingleThreadedBehavior_STA = 0x190001,
+        ComDefaultExceptionHandling_HandleAll = 0x1A0000,
+        ComDefaultExceptionHandling_HandleNone = 0x1A0001,
+        ComOopProxyAgility_Agile = 0x1B0000,
+        ComOopProxyAgility_NonAgile = 0x1B0001,
+        AppServiceLifetime_StandardTimeout = 0x1C0000,
+        AppServiceLifetime_ExtendedForSamePackage = 0x1C0001,
+        WebPlatform_Edge = 0x1D0000,
+        WebPlatform_Legacy = 0x1D0001,
+        WinInetStoragePartitioning_Isolated = 0x1E0000,
+        WinInetStoragePartitioning_SharedWithAppContainer = 0x1E0001,
+        IndexerProtocolHandlerHost_PerUser = 0x1F0000,
+        IndexerProtocolHandlerHost_PerApp = 0x1F0001,
+        LoaderIncludeUserDirectories_False = 0x200000,
+        LoaderIncludeUserDirectories_True = 0x200001,
+        ConvertAppContainerToRestrictedAppContainer_False = 0x210000,
+        ConvertAppContainerToRestrictedAppContainer_True = 0x210001,
+        PackageMayContainPrivateMapiProvider_None = 0x220000,
+        PackageMayContainPrivateMapiProvider_PrivateHive = 0x220001,
+        AdminProcessPackageClaims_None = 0x230000,
+        AdminProcessPackageClaims_Caller = 0x230001,
+        RegistryRedirectionBehavior_None = 0x240000,
+        RegistryRedirectionBehavior_CopyOnWrite = 0x240001,
+        BypassCreateProcessAppxExtension_False = 0x250000,
+        BypassCreateProcessAppxExtension_True = 0x250001,
+        KnownFolderRedirection_Isolated = 0x260000,
+        KnownFolderRedirection_RedirectToPackage = 0x260001,
+        PrivateActivateAsPackageWinrtClasses_AllowNone = 0x270000,
+        PrivateActivateAsPackageWinrtClasses_AllowFullTrust = 0x270001,
+        PrivateActivateAsPackageWinrtClasses_AllowNonFullTrust = 0x270002,
+        AppPrivateFolderRedirection_None = 0x280000,
+        AppPrivateFolderRedirection_AppPrivate = 0x280001,
+        GlobalSystemAppDataAccess_Normal = 0x290000,
+        GlobalSystemAppDataAccess_Virtualized = 0x290001,
+        ConsoleHandleInheritance_ConsoleOnly = 0x2A0000,
+        ConsoleHandleInheritance_All = 0x2A0001,
+        ConsoleBufferAccess_RestrictedUnidirectional = 0x2B0000,
+        ConsoleBufferAccess_Unrestricted = 0x2B0001,
+        ConvertCallerTokenToUserTokenForDeployment_UserCallerToken = 0x2C0000,
+        ConvertCallerTokenToUserTokenForDeployment_ConvertTokenToUserToken = 0x2C0001,
+    };
+
     public static partial class NtRtl
     {
         public const uint SecurityDescriptorRevision = 1;
@@ -478,6 +634,17 @@ namespace NtApiDotNet
 
         [DllImport("ntdll.dll")]
         public static extern NtStatus RtlCheckSandboxedToken(SafeKernelObjectHandle token, out bool is_sandboxed);
+
+        [DllImport("ntdll.dll", CharSet = CharSet.Unicode)]
+        public static extern NtStatus RtlQueryPackageClaims(
+            SafeKernelObjectHandle TokenHandle,
+            [In, Out] byte[] PackageFullName,
+            [In, Out] OptionalLength PackageSize, // Size in bytes.
+            [In, Out] byte[] AppId,
+            [In, Out] OptionalLength AppIdSize,   // Size in bytes.
+            [Out] OptionalGuid DynamicId,
+            [Out] PsPkgClaim PkgClaim,
+            OptionalInt64 AttributesPresent);
     }
 
     public static partial class NtSystemCalls
