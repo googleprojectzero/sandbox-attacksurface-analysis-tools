3ad55c9452d469a507286968985c264dc7e2c7c1
James Forshaw <tyranid@gmail.com>
2018-10-16 17:55:19 +0200

    Removed unused using.

--- NtApiDotNet/SecurityCapabilities.cs
+++ NtApiDotNet/SecurityCapabilities.cs
@@ -12,9 +12,6 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-using System;
-using System.Collections.Generic;
-
 namespace NtApiDotNet
 {
     internal static class SecurityCapabilities
