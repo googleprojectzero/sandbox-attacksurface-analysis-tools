043afbbe15cd0f3262dbe643de02df5843618d82
James Forshaw <tyranid@gmail.com>
2019-03-02 16:30:13 +0530

    Updated the flags.

--- NtApiDotNet/Win32/SafeLoadLibraryHandle.cs
+++ NtApiDotNet/Win32/SafeLoadLibraryHandle.cs
@@ -39,25 +39,45 @@ namespace NtApiDotNet.Win32
         /// </summary>
         DontResolveDllReferences = 0x00000001,
         /// <summary>
-        /// Ignore code authz level.
-        /// </summary>
-        LoadIgnoreCodeAuthzLevel = 0x00000010,
-        /// <summary>
         /// Load library as a data file.
         /// </summary>
         LoadLibraryAsDataFile = 0x00000002,
         /// <summary>
-        /// Load library as a data file exclusively.
+        /// Load with an altered search path.
         /// </summary>
-        LoadLibraryAsDataFileExclusive = 0x00000040,
+        LoadWithAlteredSearchPath = 0x00000008,
+        /// <summary>
+        /// Ignore code authz level.
+        /// </summary>
+        LoadIgnoreCodeAuthzLevel = 0x00000010,
         /// <summary>
         /// Load library as an image resource.
         /// </summary>
         LoadLibraryAsImageResource = 0x00000020,
         /// <summary>
-        /// Load with an altered search path.
+        /// Load library as a data file exclusively.
         /// </summary>
-        LoadWithAlteredSearchPath = 0x00000008
+        LoadLibraryAsDataFileExclusive = 0x00000040,
+        /// <summary>
+        /// Add the DLL's directory temporarily to the search list.
+        /// </summary>
+        LoadLibrarySearchDllLoadDir = 0x00000100,
+        /// <summary>
+        /// Search application directory for the DLL.
+        /// </summary>
+        LoadLibrarySearchApplicationDir = 0x00000200,
+        /// <summary>
+        /// Search the user's directories for the DLL.
+        /// </summary>
+        LoadLibrarySearchUserDirs = 0x00000400,
+        /// <summary>
+        /// Search system32 for the DLL.
+        /// </summary>
+        LoadLibrarySearchSystem32 = 0x00000800,
+        /// <summary>
+        /// Search the default directories for the DLL.
+        /// </summary>
+        LoadLibrarySearchDefaultDirs = 0x00001000,
     }
 
     /// <summary>
