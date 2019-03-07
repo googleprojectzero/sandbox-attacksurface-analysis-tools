5d027c32b7da608f4ae3d098ee9fc8e15a36d84c
James Forshaw <tyranid@gmail.com>
2019-03-07 22:59:23 +0000

    Added New-NtAlpcSendAttributes.

--- NtObjectManager/NtAlpcCmdlets.cs
+++ NtObjectManager/NtAlpcCmdlets.cs
@@ -14,6 +14,7 @@
 
 using NtApiDotNet;
 using System;
+using System.Collections;
 using System.Management.Automation;
 using System.Text;
 
@@ -622,7 +623,7 @@ namespace NtObjectManager
     /// </summary>
     /// <example>
     ///   <code>$attrs = New-NtAlpcReceiveAttributes</code>
-    ///   <para>Create a new receive attributes buffer with space for all known attributes..</para>
+    ///   <para>Create a new receive attributes buffer with space for all known attributes.</para>
     /// </example>
     /// <example>
     ///   <code>$attrs = New-NtAlpcReceiveAttributes -Attributes View, Context</code>
@@ -634,7 +635,7 @@ namespace NtObjectManager
     public class NewNtAlpcReceiveAttributes : PSCmdlet
     {
         /// <summary>
-        /// <para type="description">Create the message from a byte array.</para>
+        /// <para type="description">Specify the list of attributes for the receive buffer.</para>
         /// </summary>
         [Parameter(Position = 0)]
         public AlpcMessageAttributeFlags Attributes { get; set; }
@@ -653,6 +654,117 @@ namespace NtObjectManager
         protected override void ProcessRecord()
         {
             WriteObject(new AlpcReceiveMessageAttributes(Attributes));
+        }
+    }
+
+    /// <summary>
+    /// <para type="synopsis">Creates a new send attributes buffer.</para>
+    /// <para type="description">This cmdlet creates a new send attributes buffer. The buffer can be initialized with a list of attributes or by specifying specific values.</para>
+    /// </summary>
+    /// <example>
+    ///   <code>$attrs = New-NtAlpcSendAttributes</code>
+    ///   <para>Create a new empty send attributes buffer.</para>
+    /// </example>
+    /// <example>
+    ///   <code>$attrs = New-NtAlpcSendAttributes -Attributes $view, $handle</code>
+    ///   <para>Create a new send attributes buffer with view and handle attribute objects.</para>
+    /// </example>
+    /// <example>
+    ///   <code>$attrs = New-NtAlpcSendAttributes -Object $proc</code>
+    ///   <para>Create a new send attributes buffer with a handle attribute from a process handle.</para>
+    /// </example>
+    /// <example>
+    ///   <code>$attrs = New-NtAlpcSendAttributes -WorkOnBehalfOf</code>
+    ///   <para>Create a new send attributes buffer with a Work on Behalf of attribute.</para>
+    /// </example>
+    /// <example>
+    ///   <code>$attrs = New-NtAlpcSendAttributes -PortSection $section</code>
+    ///   <para>Create a new send attributes buffer with data view from an allocated port section.</para>
+    /// </example>
+    [Cmdlet(VerbsCommon.New, "NtAlpcSendAttributes", DefaultParameterSetName = "FromAttributes")]
+    [OutputType(typeof(AlpcSendMessageAttributes))]
+    public class NewNtAlpcSendAttributes : PSCmdlet
+    {
+        /// <summary>
+        /// <para type="description">Specify the list of attributes for the send buffer.</para>
+        /// </summary>
+        [Parameter(Position = 0, ParameterSetName = "FromAttributes")]
+        public AlpcMessageAttribute[] Attributes { get; set; }
+
+        /// <summary>
+        /// <para type="description">Create a handle attribute from a list of objects.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromParts")]
+        public NtObject[] Object { get; set; }
+
+        /// <summary>
+        /// <para type="description">Create a handle attribute from a list of handle entries.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromParts")]
+        public AlpcHandleMessageAttributeEntry[] Handle { get; set; }
+
+        /// <summary>
+        /// <para type="description">Add a Work on Behalf of attribute.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromParts")]
+        public SwitchParameter WorkOnBehalfOf { get; set; }
+
+        /// <summary>
+        /// <para type="description">Add a data view from an allocated port section.</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "FromParts")]
+        public AlpcPortSection PortSection { get; set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public NewNtAlpcSendAttributes()
+        {
+            Attributes = new AlpcMessageAttribute[0];
+            Object = new NtObject[0];
+            Handle = new AlpcHandleMessageAttributeEntry[0];
+        }
+
+        private AlpcSendMessageAttributes CreateFromParts()
+        {
+            var attrs = new AlpcSendMessageAttributes();
+            if (Object.Length > 0)
+            {
+                attrs.AddHandles(Object);
+            }
+
+            if (Handle.Length > 0)
+            {
+                attrs.AddHandles(Handle);
+            }
+
+            if (WorkOnBehalfOf)
+            {
+                attrs.Add(new AlpcWorkOnBehalfMessageAttribute());
+            }
+
+            if (PortSection != null)
+            {
+                attrs.Add(PortSection.CreateSectionView());
+            }
+
+            return attrs;
+        }
+
+        /// <summary>
+        /// Process record.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            switch (ParameterSetName)
+            {
+                case "FromAttributes":
+                    WriteObject(new AlpcSendMessageAttributes(Attributes));
+                    break;
+                case "FromParts":
+                    WriteObject(CreateFromParts());
+                    break;
+            }
         }
     }
 }
