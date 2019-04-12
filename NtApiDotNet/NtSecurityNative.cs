d1bc5903c512daec7d31a970b81533bcafefe3b8
James Forshaw <tyranid@gmail.com>
2019-04-12 17:51:46 +0100

    Moved security descriptor internals to a more sensible location.

--- NtApiDotNet/NtSecurityNative.cs
+++ NtApiDotNet/NtSecurityNative.cs
@@ -787,5 +787,153 @@ namespace NtApiDotNet
         Child
     }
 
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SecurityDescriptorHeader
+    {
+        public byte Revision;
+        public byte Sbz1;
+        public SecurityDescriptorControl Control;
+
+        public bool HasFlag(SecurityDescriptorControl control)
+        {
+            return (control & Control) == control;
+        }
+    }
+
+    internal interface ISecurityDescriptor
+    {
+        long GetOwner(long base_address);
+        long GetGroup(long base_address);
+        long GetSacl(long base_address);
+        long GetDacl(long base_address);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SecurityDescriptorRelative : ISecurityDescriptor
+    {
+        public SecurityDescriptorHeader Header;
+        public int Owner;
+        public int Group;
+        public int Sacl;
+        public int Dacl;
+
+        long ISecurityDescriptor.GetOwner(long base_address)
+        {
+            if (Owner == 0)
+            {
+                return 0;
+            }
+
+            return base_address + Owner;
+        }
+
+        long ISecurityDescriptor.GetGroup(long base_address)
+        {
+            if (Group == 0)
+            {
+                return 0;
+            }
+
+            return base_address + Group;
+        }
+
+        long ISecurityDescriptor.GetSacl(long base_address)
+        {
+            if (Sacl == 0)
+            {
+                return 0;
+            }
+
+            return base_address + Sacl;
+        }
+
+        long ISecurityDescriptor.GetDacl(long base_address)
+        {
+            if (Dacl == 0)
+            {
+                return 0;
+            }
+
+            return base_address + Dacl;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SecurityDescriptorAbsolute : ISecurityDescriptor
+    {
+        public SecurityDescriptorHeader Header;
+        public IntPtr Owner;
+        public IntPtr Group;
+        public IntPtr Sacl;
+        public IntPtr Dacl;
+
+        long ISecurityDescriptor.GetOwner(long base_address)
+        {
+            return Owner.ToInt64();
+        }
+
+        long ISecurityDescriptor.GetGroup(long base_address)
+        {
+            return Group.ToInt64();
+        }
+
+        long ISecurityDescriptor.GetSacl(long base_address)
+        {
+            return Sacl.ToInt64();
+        }
+
+        long ISecurityDescriptor.GetDacl(long base_address)
+        {
+            return Dacl.ToInt64();
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SecurityDescriptorAbsolute32 : ISecurityDescriptor
+    {
+        public SecurityDescriptorHeader Header;
+        public int Owner;
+        public int Group;
+        public int Sacl;
+        public int Dacl;
+
+        long ISecurityDescriptor.GetOwner(long base_address)
+        {
+            return Owner;
+        }
+
+        long ISecurityDescriptor.GetGroup(long base_address)
+        {
+            return Group;
+        }
+
+        long ISecurityDescriptor.GetSacl(long base_address)
+        {
+            return Sacl;
+        }
+
+        long ISecurityDescriptor.GetDacl(long base_address)
+        {
+            return Dacl;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SidHeader
+    {
+        public byte Revision;
+        public byte RidCount;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct AclHeader
+    {
+        public byte AclRevision;
+        public byte Sbz1;
+        public ushort AclSize;
+        public ushort AceCount;
+        public ushort Sbz2;
+    }
+
 #pragma warning restore 1591
 }
