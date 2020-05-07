341cba8363dd1bb629ac9c372d8b8bd7e2770965
James Forshaw <tyranid@gmail.com>
2020-05-07 19:07:09 +0100

    Added basic ARC4 implementation.

---
+++ NtApiDotNet/Utilities/Security/ARC4.cs
@@ -0,0 +1,69 @@
+﻿//  Copyright 2020 Google Inc. All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+using System.Linq;
+
+namespace NtApiDotNet.Utilities.Security
+{
+    /// <summary>
+    /// Basic implementation of ARC4.
+    /// </summary>
+    public static class ARC4
+    {
+        private static void Swap(byte[] s, int i, int j)
+        {
+            byte x = s[i];
+            s[i] = s[j];
+            s[j] = x;
+        }
+
+        private static byte[] CreateKeySchedule(byte[] key)
+        {
+            byte[] s = Enumerable.Range(0, 256).Select(i => (byte)i).ToArray();
+            int j = 0;
+            for (int i = 0; i < 256; ++i)
+            {
+                j = (j + s[i] + key[i % key.Length]) & 0xFF;
+                Swap(s, i, j);
+            }
+            return s;
+        }
+
+        /// <summary>
+        /// Encrypt, or decrypt an ARC4 stream.
+        /// </summary>
+        /// <param name="data">The data to encrypt/decrypt.</param>
+        /// <param name="key">The key.</param>
+        /// <returns>The resulting bytes.</returns>
+        public static byte[] Encrypt(byte[] data, byte[] key)
+        {
+            byte[] s = CreateKeySchedule(key);
+            byte[] ret = (byte[])data.Clone();
+
+            int i = 0;
+            int j = 0;
+            int p = 0;
+            while (p < data.Length)
+            {
+                i = (i + 1) & 0xFF;
+                j = (j + s[i]) & 0xFF;
+                Swap(s, i, j);
+                byte k = s[(s[i] + s[j]) & 0xFF];
+                ret[p] = (byte)(data[p] ^ k);
+                p++;
+            }
+            return ret;
+        }
+    }
+}
--- NtApiDotNet/Win32/Security/Authentication/Kerberos/KerberosKey.cs
+++ NtApiDotNet/Win32/Security/Authentication/Kerberos/KerberosKey.cs
@@ -16,7 +16,6 @@ using NtApiDotNet.Utilities.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 
