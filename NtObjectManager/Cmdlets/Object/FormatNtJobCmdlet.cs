945fd61bfa4db8abc883a0a0b4d708161263e9b4
James Forshaw <tyranid@gmail.com>
2020-10-06 22:02:55 +0100

    Added additional silo properties.

--- NtObjectManager/Cmdlets/Object/FormatNtJobCmdlet.cs
+++ NtObjectManager/Cmdlets/Object/FormatNtJobCmdlet.cs
@@ -158,6 +158,8 @@ namespace NtObjectManager.Cmdlets.Object
             {
                 WriteObject($"Root Directory: {root_dir}");
             }
+            WriteObject($"Container ID  : {job.ContainerId}");
+            WriteObject($"Telemetry ID  : {job.ContainerTelemetryId}");
             WriteObject($"Impersonation : {(job.ThreadImpersonation ? "Enabled" : "Disabled")}");
             WriteObject(string.Empty);
             if (!basic_info.Result.IsInServerSilo)
