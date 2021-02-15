1fe0efc8040838cd58ee1a17fe6a56652b81c153
James Forshaw <tyranid@gmail.com>
2021-02-14 20:32:43 -0800

    Tokenize the digest auth token.

--- NtApiDotNet/Win32/Security/Authentication/Digest/DigestAuthenticationToken.cs
+++ NtApiDotNet/Win32/Security/Authentication/Digest/DigestAuthenticationToken.cs
@@ -13,6 +13,7 @@
 //  limitations under the License.
 
 using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace NtApiDotNet.Win32.Security.Authentication.Digest
@@ -22,6 +23,8 @@ namespace NtApiDotNet.Win32.Security.Authentication.Digest
     /// </summary>
     public sealed class DigestAuthenticationToken : AuthenticationToken
     {
+        private readonly Lazy<List<string>> _split_token;
+
         /// <summary>
         /// The digest token as a string.
         /// </summary>
@@ -33,12 +36,41 @@ namespace NtApiDotNet.Win32.Security.Authentication.Digest
         /// <returns></returns>
         public override string Format()
         {
-            return Data;
+            StringBuilder builder = new StringBuilder();
+            foreach (var p in _split_token.Value)
+            {
+                builder.AppendLine(p);
+            }
+
+            return builder.ToString();
         }
 
         private DigestAuthenticationToken(byte[] data) : base(data)
         {
             Data = Encoding.UTF8.GetString(data);
+            _split_token = new Lazy<List<string>>(SplitToken);
+        }
+
+        private List<string> SplitToken()
+        {
+            int start_index = 0;
+            bool in_quote = false;
+            List<string> ret = new List<string>();
+
+            for (int i = 0; i < Data.Length - 1; ++i)
+            {
+                if (Data[i] == ',' && !in_quote)
+                {
+                    ret.Add(Data.Substring(start_index, i - start_index));
+                    start_index = i + 1;
+                }
+                else if (Data[i] == '"')
+                {
+                    in_quote = !in_quote;
+                }
+            }
+            ret.Add(Data.Substring(start_index));
+            return ret;
         }
 
         internal static bool TryParse(byte[] data, out DigestAuthenticationToken token)
