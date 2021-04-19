b1bf32a575c05805c5893083f55159e83f614262
James Forshaw <tyranid@gmail.com>
2021-04-18 19:56:01 -0700

    Added additional property for key entry.

--- NtObjectManager/Provider/NtKeyEntry.cs
+++ NtObjectManager/Provider/NtKeyEntry.cs
@@ -13,7 +13,9 @@
 //  limitations under the License.
 
 using NtApiDotNet;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NtObjectManager.Provider
 {
@@ -22,20 +24,34 @@ namespace NtObjectManager.Provider
     /// </summary>
     public sealed class NtKeyEntry : NtDirectoryEntry
     {
-        private List<NtKeyValue> _values;
+        private Dictionary<string, NtKeyValue> _value_dict;
         private KeyControlFlags _control_flags;
         private KeyVirtualizationFlags _virtualization_flags;
+        private string _class_name;
         private readonly bool _open_for_backup;
 
         private protected override void PopulateKeyData(NtKey key)
         {
             base.PopulateKeyData(key);
-            if (key.IsAccessGranted(KeyAccessRights.QueryValue))
+            try
             {
-                _values = new List<NtKeyValue>(key.QueryValues());
+                if (key.IsAccessGranted(KeyAccessRights.QueryValue))
+                {
+                    _value_dict = key.QueryValues().ToDictionary(v => v.Name, StringComparer.OrdinalIgnoreCase);
+                }
             }
+            catch
+            {
+            }
+            _class_name = key.ClassName;
             _control_flags = key.ControlFlags;
             _virtualization_flags = key.VirtualizationFlags;
+        }
+
+        private Dictionary<string, NtKeyValue> GetValueDict()
+        {
+            PopulateData();
+            return _value_dict ?? new Dictionary<string, NtKeyValue>();
         }
 
         /// <summary>
@@ -48,8 +64,8 @@ namespace NtObjectManager.Provider
         {
             using (var obja = new ObjectAttributes(RelativePath, AttributeFlags.OpenLink | AttributeFlags.CaseInsensitive, _root))
             {
-                return NtKey.Open(obja, KeyAccessRights.MaximumAllowed, 
-                    _open_for_backup ? KeyCreateOptions.BackupRestore : KeyCreateOptions.NonVolatile, 
+                return NtKey.Open(obja, KeyAccessRights.MaximumAllowed,
+                    _open_for_backup ? KeyCreateOptions.BackupRestore : KeyCreateOptions.NonVolatile,
                     throw_on_error).Cast<NtObject>();
             }
         }
@@ -63,34 +79,29 @@ namespace NtObjectManager.Provider
         /// <summary>
         /// Get the key's values.
         /// </summary>
-        public IEnumerable<NtKeyValue> Values
-        {
-            get
-            {
-                if (_values == null)
-                {
-                    _values = new List<NtKeyValue>();
-                    PopulateData();
-                }
-                return _values.AsReadOnly();
-            }
-        }
+        public IEnumerable<NtKeyValue> Values => GetValueDict().Values;
+
+        /// <summary>
+        /// Get names of the values.
+        /// </summary>
+        public IEnumerable<string> ValueNames => Values.Select(v => v.Name);
 
         /// <summary>
         /// Get the number of values in the key.
         /// </summary>
-        public int ValueCount
-        {
-            get
-            {
-                if (_values == null)
-                {
-                    _values = new List<NtKeyValue>();
-                    PopulateData();
-                }
-                return _values.Count;
-            }
-        }
+        public int ValueCount => GetValueDict().Count;
+
+        /// <summary>
+        /// Get a key by name.
+        /// </summary>
+        /// <param name="name">The name of the key.</param>
+        /// <returns>Returns the key if found or null.</returns>
+        public NtKeyValue this[string name] => GetValueDict().TryGetValue(name, out NtKeyValue value) ? value : null;
+
+        /// <summary>
+        /// Get the default value for the key.
+        /// </summary>
+        public NtKeyValue DefaultValue => this[string.Empty];
 
         /// <summary>
         /// Get Key Control Flags.
@@ -99,12 +110,20 @@ namespace NtObjectManager.Provider
         {
             get
             {
-                if (_values == null)
-                {
-                    _values = new List<NtKeyValue>();
-                    PopulateData();
-                }
+                PopulateData();
                 return _control_flags;
+            }
+        }
+
+        /// <summary>
+        /// Get key class name.
+        /// </summary>
+        public string ClassName
+        {
+            get
+            {
+                PopulateData();
+                return _class_name;
             }
         }
 
@@ -115,11 +134,7 @@ namespace NtObjectManager.Provider
         {
             get
             {
-                if (_values == null)
-                {
-                    _values = new List<NtKeyValue>();
-                    PopulateData();
-                }
+                PopulateData();
                 return _virtualization_flags;
             }
         }
