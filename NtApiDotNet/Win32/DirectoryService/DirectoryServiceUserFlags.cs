a85b8edf5295e5a6819bf7291d330a8335e346ef
James Forshaw <tyranid@gmail.com>
2021-07-30 06:48:08 -0700

    Renamed flags.

--- NtApiDotNet/Win32/DirectoryService/DirectoryServiceUserFlags.cs
+++ NtApiDotNet/Win32/DirectoryService/DirectoryServiceUserFlags.cs
@@ -20,58 +20,59 @@ namespace NtApiDotNet.Win32.DirectoryService
     /// <summary>
     /// User flags.
     /// </summary>
+    [SDKName("ADS_USER_FLAG")]
     [Flags]
-    public enum DirectoryServiceUserAccountControlFlags : uint
+    public enum DirectoryServiceUserFlags : uint
     {
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         None = 0,
-        [SDKName("UF_SCRIPT")]
+        [SDKName("ADS_UF_SCRIPT")]
         Script = 0x00000001,
-        [SDKName("UF_ACCOUNTDISABLE")]
+        [SDKName("ADS_UF_ACCOUNTDISABLE")]
         AccountDisable = 0x00000002,
-        [SDKName("UF_HOMEDIR_REQUIRED")]
+        [SDKName("ADS_UF_HOMEDIR_REQUIRED")]
         HomedirRequired = 0x00000008,
-        [SDKName("UF_LOCKOUT")]
+        [SDKName("ADS_UF_LOCKOUT")]
         Lockout = 0x00000010,
-        [SDKName("UF_PASSWD_NOTREQD")]
+        [SDKName("ADS_UF_PASSWD_NOTREQD")]
         PasswordNotRequired = 0x00000020,
-        [SDKName("UF_PASSWD_CANT_CHANGE")]
+        [SDKName("ADS_UF_PASSWD_CANT_CHANGE")]
         PasswordCantChange = 0x00000040,
-        [SDKName("UF_ENCRYPTED_TEXT_PASSWORD_ALLOWED")]
+        [SDKName("ADS_UF_ENCRYPTED_TEXT_PASSWORD_ALLOWED")]
         EncryptedTextPasswordAllowed = 0x00000080,
-        [SDKName("UF_TEMP_DUPLICATE_ACCOUNT")]
+        [SDKName("ADS_UF_TEMP_DUPLICATE_ACCOUNT")]
         TempDuplicateAccount = 0x00000100,
-        [SDKName("UF_NORMAL_ACCOUNT")]
+        [SDKName("ADS_UF_NORMAL_ACCOUNT")]
         NormalAccount = 0x00000200,
-        [SDKName("UF_INTERDOMAIN_TRUST_ACCOUNT")]
+        [SDKName("ADS_UF_INTERDOMAIN_TRUST_ACCOUNT")]
         InterDomainTrustAccount = 0x00000800,
-        [SDKName("UF_WORKSTATION_TRUST_ACCOUNT")]
+        [SDKName("ADS_UF_WORKSTATION_TRUST_ACCOUNT")]
         WorkstationTrustAccount = 0x00001000,
-        [SDKName("UF_SERVER_TRUST_ACCOUNT")]
+        [SDKName("ADS_UF_SERVER_TRUST_ACCOUNT")]
         ServerTrustAccount = 0x00002000,
-        [SDKName("UF_DONT_EXPIRE_PASSWD")]
+        [SDKName("ADS_UF_DONT_EXPIRE_PASSWD")]
         DontExpirePassword = 0x00010000,
-        [SDKName("UF_MNS_LOGON_ACCOUNT")]
+        [SDKName("ADS_UF_MNS_LOGON_ACCOUNT")]
         MNSLogonAccount = 0x00020000,
-        [SDKName("UF_SMARTCARD_REQUIRED")]
+        [SDKName("ADS_UF_SMARTCARD_REQUIRED")]
         SmartcardRequired = 0x00040000,
-        [SDKName("UF_TRUSTED_FOR_DELEGATION")]
+        [SDKName("ADS_UF_TRUSTED_FOR_DELEGATION")]
         TrustedForDelegation = 0x00080000,
-        [SDKName("UF_NOT_DELEGATED")]
+        [SDKName("ADS_UF_NOT_DELEGATED")]
         NotDelegated = 0x00100000,
-        [SDKName("UF_USE_DES_KEY_ONLY")]
+        [SDKName("ADS_UF_USE_DES_KEY_ONLY")]
         UseDesKeyOnly = 0x00200000,
-        [SDKName("UF_DONT_REQUIRE_PREAUTH")]
+        [SDKName("ADS_UF_DONT_REQUIRE_PREAUTH")]
         DontRequirePreAuth = 0x00400000,
-        [SDKName("UF_PASSWORD_EXPIRED")]
+        [SDKName("ADS_UF_PASSWORD_EXPIRED")]
         PasswordExpired = 0x00800000,
-        [SDKName("UF_TRUSTED_TO_AUTHENTICATE_FOR_DELEGATION")]
+        [SDKName("ADS_UF_TRUSTED_TO_AUTHENTICATE_FOR_DELEGATION")]
         TrustedToAuthenticateForDelegation = 0x01000000,
-        [SDKName("UF_NO_AUTH_DATA_REQUIRED")]
+        [SDKName("ADS_UF_NO_AUTH_DATA_REQUIRED")]
         NoAuthDataRequired = 0x02000000,
-        [SDKName("UF_PARTIAL_SECRETS_ACCOUNT")]
+        [SDKName("ADS_UF_PARTIAL_SECRETS_ACCOUNT")]
         PartialSecretsAccount = 0x04000000,
-        [SDKName("UF_USE_AES_KEYS")]
+        [SDKName("ADS_UF_USE_AES_KEYS")]
         UseAesKeys = 0x08000000
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     }
