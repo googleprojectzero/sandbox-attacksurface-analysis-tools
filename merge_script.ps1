3094378033a67023f054c2151f9a1938a2ae76e8
James Forshaw <tyranid@gmail.com>
2021-08-17 20:51:02 -0700

    Added simple script to merge powershell module.

---
+++ merge_script.ps1
@@ -0,0 +1,38 @@
+﻿Set-StrictMode -Version Latest
+
+function Remove-LeadingComment {
+    param([string[]]$lines)
+
+    $index = 0
+    while($index -lt $lines.Count) {
+        if (!$lines[$index].StartsWith("#")) {
+            break
+        }
+        $index++
+    }
+    if ($index -lt $lines.Count) {
+        $lines[$index..($lines.Count-1)]
+    }
+}
+
+function Get-MergedScript {
+    param([string]$Path)
+    
+    try {
+        $Path = Resolve-Path $Path
+
+        $c = Get-Content -Path "$Path\NtObjectManager.psm1" | Out-String
+        $i = $c.IndexOf("# Source the external scripts into this module.")
+
+        $res = $c.Substring(0, $i)
+        $fs = ls "$Path\*.ps1"
+        foreach($f in $fs) {
+            $c = Get-Content -Path $f.FullName
+            $c = Remove-LeadingComment $c | Out-String
+            $res += $c
+        }
+        $res
+    } catch {
+        Write-Error $_
+    }
+}
