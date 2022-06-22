4a4121006f6840b91d4ee7b366748d9a4c3915b6
James Forshaw <tyranid@gmail.com>
2022-06-22 16:37:32 +0200

    Remove some using.

--- NtApiDotNet/Win32/Security/Authentication/Kerberos/KerberosKeySet.cs
+++ NtApiDotNet/Win32/Security/Authentication/Kerberos/KerberosKeySet.cs
@@ -12,7 +12,6 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-using NtApiDotNet.Win32.SafeHandles;
 using NtApiDotNet.Win32.Security.Authentication.Logon;
 using NtApiDotNet.Win32.Security.Native;
 using System;
