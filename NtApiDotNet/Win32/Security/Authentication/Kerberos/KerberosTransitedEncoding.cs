4ad0ceee53df7b777cbc0500f2ff7868e7bcc432
James Forshaw <tyranid@gmail.com>
2022-08-01 10:33:18 -0700

    Added default constructor.

--- NtApiDotNet/Win32/Security/Authentication/Kerberos/KerberosTransitedEncoding.cs
+++ NtApiDotNet/Win32/Security/Authentication/Kerberos/KerberosTransitedEncoding.cs
@@ -14,6 +14,7 @@
 
 using NtApiDotNet.Utilities.ASN1;
 using NtApiDotNet.Utilities.ASN1.Builder;
+using System;
 using System.IO;
 
 namespace NtApiDotNet.Win32.Security.Authentication.Kerberos
@@ -47,6 +48,14 @@ namespace NtApiDotNet.Win32.Security.Authentication.Kerberos
         /// Transited encoding data.
         /// </summary>
         public byte[] Data { get; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public KerberosTransitedEncoding() 
+            : this(KerberosTransitedEncodingType.X500Compress, Array.Empty<byte>())
+        {
+        }
 
         /// <summary>
         /// Constructor.
